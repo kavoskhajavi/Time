2479832c4e326be74d83528dcdb7d1f54124eecb
bfpig <bfpig@bfpigs-imac.local>
2017-06-13 11:27:45 +0400

    Initial Commit

---
+++ Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time
+//
+//  Created by bfpig on 3/23/1396 AP.
+//  Copyright © 1396 bfpig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
